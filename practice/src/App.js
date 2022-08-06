e230fa4b5107835623a115efe1891b13f5894967
안웅진 <ad1230023@gamil.com>
2022-08-06 14:34:39 +0900

    22-08-06
    리엑트 연습

---
+++ practice/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Say from './Say';
+import EventPractice from './EventPractice';
+
+const App = () => {
+  return (
+    <>
+      <Say />
+      <EventPractice />
+    </>
+  );
+}
+
+export default App;
