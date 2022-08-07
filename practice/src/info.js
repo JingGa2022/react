e0462e13e6089e671cf012abbfd442371d0b41c9
안웅진 <ad1230023@gamil.com>
2022-08-07 15:10:58 +0900

    2022-08-07
    인풋 상태관리 연습

--- practice/src/info.js
+++ practice/src/info.js
@@ -1,29 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
+
+function reducer(state, action) {
+    return {
+        ...state,
+        [action.name]: action.value
+    };
+}
 
 const Info = () => {
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
-    useEffect(() => {
-        console.log('effect');
-        console.log(name);
-        return () => {
-            console.log('cleanup');
-            console.log(name);
-        }
-    }, [name]);
-
-    const onChangeName = e => {
-        setName(e.target.value);
+    const [state, dispatch] = useReducer(reducer, {
+        name: '',
+        nickname: ''
+    });
+    const { name, nickname } = state;
+    const onChange = e => {
+        dispatch(e.target);
     }
-    const onChangeNickname = e => {
-        setNickname(e.target.value);
-    };
-
     return (
         <>
             <div>
-                <input value={name} onChange={onChangeName}></input>
-                <input value={nickname} onChange={onChangeNickname}></input>
+                <input value={name} onChange={onChange}></input>
+                <input value={nickname} onChange={onChange}></input>
             </div>
             <div>
                 <div>
