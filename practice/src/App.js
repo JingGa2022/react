524ee6de2ead683670afb04e3905673c32600adf
안웅진 <ad1230023@gamil.com>
2022-08-07 15:04:23 +0900

    22-08-07
    useState 연습

--- practice/src/App.js
+++ practice/src/App.js
@@ -4,15 +4,25 @@ import EventPractice from './EventPractice';
 import IterationSample from './IterationSample';
 import Counter from './Counter';
 import Info from './info';
+import { useState } from 'react';
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <>
       <Say />
       <EventPractice />
       <IterationSample />
-      <Counter></Counter>
       <Info />
+      <button onClick={() => {
+        setVisible(!visible);
+      }}>{visible ? 'hide' : 'show'}</button>
+      <br />
+      {visible && <Info />}
+
+
+      <Counter />
+
     </>
   );
 }
